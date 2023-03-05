f622236d3290f75a35c81083f041f87e0ff2919e
Dwaraka31 <111960889+Dwaraka31@users.noreply.github.com>
2023-03-05 16:33:40 -0500

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,16 @@
+def newgit(repo)
+{
+  git "${repo}"
+}
+def newBuild()
+{
+  sh 'maven package'
+}
+def newDeployment(path, ipAdress, contextPath)
+{
+  sh "scp /var/lib/jenkins/workspace/${Path}/webapp/target/webapp.war ubuntu@${ipAdress}:/var/lib/tomcat9/webapps/${contextPath}.war"
+ }
+ def Testing(path)
+ {
+  sh 'java -jar /var/lib/jenkins/workspace/${path}/testing.jar'
+ }
